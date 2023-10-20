5266e092b245b4374f2898e49fbabf0fff7927aa
Chi <haulinhchi1881@gmail.com>
2023-10-20 15:25:28 +0700

    .

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
-const spwrapper = document.querySelector(".sp-card-wrapper");
-arrowbtns = document.querySelectorAll (".sp-slide-container .arrow-btn");
-firstCard = spwrapper.querySelectorAll(".sp-card")[0];
+// ============= START MENU
+$(document).ready(function () {
+    $('.autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('.autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
+// ============= END MENU
 
-let isDrafStart = false, prevPageX, prevScrolleft;
-let firstCardWidth = firstCard.clientWidth + 15;
-
-//arrowbtns.forEach(btn => {
-//    btn.addEventListener("click", () => {
-//        carousel.scrollLeft += btn.id == "left"?;
-//    })
-//});
-
-const dragStart = (e) => {
-    isDrafStart = true;
-    prevPageX = e.pageX;
-    prevScrolleft = spwrapper.scrollLeft;
-}
-
-const dragging = (e) => {
-    if(!isDrafStart) return;
-    e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
-    spwrapper.scrollLeft = prevScrolleft - positionDiff;
-}
-
-const dragStop = () => {
-    isDrafStart = false;
-}
-
-spwrapper.addEventListener("mousedown", dragStart);
-spwrapper.addEventListener("mousemove", dragging);
-spwrapper.addEventListener("mouseup", dragStop);
-
-
-
+// SlIDER
