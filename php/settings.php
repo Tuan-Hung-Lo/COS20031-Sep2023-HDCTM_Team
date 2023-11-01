04531b59ddf83add1cbb0ddc5280b6d55d80a3bd
lcduc <luongchiduc6@gmail.com>
2023-11-01 15:21:42 +0700

    .

---
+++ php/settings.php
@@ -0,0 +1,6 @@
+<?php
+    $dbHost = "feenix-mariadb.swin.edu.au";
+    $dbUser = "s104222248";
+    $dbPassword = "031104";
+    $dbName = "s104222248_db";
+?>
--- settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $host = "feenix-mariadb.swin.edu.au";
-    $user = "s104222248";
-    $pwd = "031104";
-    $sql_db = "s104222248_db";
-?>
