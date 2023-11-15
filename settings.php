e6e080a1b915075873873dd3d22d1a3b84327bd4
Tuan Hung Lo <lotuanhung.work@gmail.com>
2023-11-15 23:24:08 +0700

    .;

--- settings.php
+++ settings.php
@@ -1,11 +1,11 @@
 <?php
     $host = "feenix-mariadb.swin.edu.au";
-    $user = "s104181721";
+    $username = "s104181721";
     $pwd = "Bo0147";
     $sql_db = "s104181721_db";
 
     // Create a database connection
-    $conn = @mysqli_connect($host, $username, $password, $database);
+    $conn = @mysqli_connect($host, $username, $pwd, $sql_db);
 
     // Check the connection
     if ($conn->connect_error) {
