466274d98cc94ac43f8691a86769066c3c8d75e1
Tuan Hung Lo <lotuanhung.work@gmail.com>
2023-11-16 23:12:17 +0700

    Update jobseeker.php

--- jobseeker.php
+++ jobseeker.php
@@ -462,7 +462,7 @@
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="courses.php">Courses</a></li>
-            <li><a href="jobopportunities.html">Job Opportunities</a></li>
+            <li><a href="jobopportunities.php">Job Opportunities</a></li>
           </ul>
         </div>
 
