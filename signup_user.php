d0005dc62976e258bf9b1ae691ae0d7501b1c561
lcduc <luongchiduc6@gmail.com>
2023-11-17 00:55:00 +0700

    .

--- signup_user.php
+++ signup_user.php
@@ -12,17 +12,13 @@
     $hashed_password = password_hash($pwd, PASSWORD_DEFAULT);
 
     // Insert data into tables
-    $sql1 = "INSERT INTO s104181721_db.Users (FirstName, LastName)
+    $sql1 = "INSERT INTO s104181721_db.JobSeeker (FirstName, LastName)
         VALUES ('$first_name', '$first_name')";
     $conn->query($sql1);
 
-    $sql2 = "INSERT INTO s104181721_db.UserAuthentication (UserEmail, UserPassword)
-        VALUES ('$email', '$hashed_password')";
+    $sql2 = "INSERT INTO s104181721_db.UserAuthentication (UserEmail, UserPassword, UserRole)
+        VALUES ('$email', '$hashed_password', 'User')";
     $conn->query($sql2);
-
-    $sql3 = "INSERT INTO s104181721_db.UserRole (UserRoleName)
-        VALUES ('User')";
-    $conn->query($sql3);
 
     // Get the UserAuthenticationID after inserting data into UserAuthentication table
     $UserAuthenticationID = $conn->insert_id;
